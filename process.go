5ccbba2dd35df134894fedbd00c5a690a05ced56
Ronald G. Minnich <rminnich@gmail.com>
2015-07-18 19:26:55 -0700

    process: ask loop
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- process.go
+++ process.go
@@ -6,8 +6,22 @@ import (
 	"io/ioutil"
 	"log"
 	"encoding/csv"
+	"os"
 )
 
+var lines [][]string
+
+func done(){
+//	if err := ioutil.WriteFile("PORT_TO_HARVEY_NEXT.csv", []byte(lines[:][:]), 0666); err != nil {
+//		log.Fatalf("%v", err)
+//	}
+	c := csv.NewWriter(os.Stdout)
+	if err := c.WriteAll(lines); err != nil {
+		log.Fatalf("%v", err)
+	}
+	os.Exit(0)
+}
+		
 func main() {
 	b, err := ioutil.ReadFile("PORT_TO_HARVEY.csv")
 	if err != nil {
@@ -15,15 +29,42 @@ func main() {
 	}
 	r := bytes.NewReader(b)
 	c := csv.NewReader(r)
-	lines, err := c.ReadAll()
+	lines, err = c.ReadAll()
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
 	fmt.Printf("%v\n", lines)
 	for i := range lines {
+		var b string
 		if lines[i][1] != "" {
 			continue
 		}
 		fmt.Printf("ASK ABOUT %v\n", lines[i])
+		n, err := fmt.Scanln(&b)
+		if n == 0 {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+		switch b {
+			case "ignore":
+				lines[i][1] = "ignore"
+			case "skip":
+			case "fixed":
+				lines[i][1] = "fixed"
+			case "edit":
+				fmt.Print("Run an editor")
+			case "copy":
+				fmt.Printf("Copy %v somewhere", lines[i][0])
+				lines[i][1] = "copy"
+			case "exit":
+				done()
+			default:
+				fmt.Printf("?")
+		}
+
+
 	}
+	done()
 }
