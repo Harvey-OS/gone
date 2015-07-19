9042ae1c6cbd5b4ef8a25f913181db97db78fbd2
Ronald G. Minnich <rminnich@gmail.com>
2015-07-18 20:03:14 -0700

    Now does the copy. All that's left is the edit.
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- process.go
+++ process.go
@@ -7,14 +7,12 @@ import (
 	"log"
 	"encoding/csv"
 	"os"
+	"strings"
 )
 
 var lines [][]string
 
 func done(){
-//	if err := ioutil.WriteFile("PORT_TO_HARVEY_NEXT.csv", []byte(lines[:][:]), 0666); err != nil {
-//		log.Fatalf("%v", err)
-//	}
 	c := csv.NewWriter(os.Stdout)
 	if err := c.WriteAll(lines); err != nil {
 		log.Fatalf("%v", err)
@@ -56,7 +54,23 @@ func main() {
 			case "edit":
 				fmt.Print("Run an editor")
 			case "copy":
-				fmt.Printf("Copy %v somewhere", lines[i][0])
+				fmt.Printf("Copy %v ", lines[i][0])
+				b, err := ioutil.ReadFile(lines[i][0])
+				if err != nil {
+					log.Printf("%v", err)
+					done()
+				}
+				if ! strings.Contains(lines[i][0], "plan9.go") {
+					log.Printf("Can't split %v around plan9 9", lines[i][0])
+					done()
+				}
+				n := strings.SplitN(lines[i][0], "plan9.go", 2)
+				newname := n[0] + "harvey.go"
+				fmt.Printf("to %v ", newname)
+				if err := ioutil.WriteFile(newname, b, 0666); err != nil {
+					log.Printf("%v", err)
+					done()
+				}
 				lines[i][1] = "copy"
 			case "exit":
 				done()
