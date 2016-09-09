17049c8ee6d6b6ede85e3f6d87149c3cde4485c7
Ronald G. Minnich <rminnich@gmail.com>
2016-09-09 09:01:14 -0700

    HARVEY: minimal changes to make harvey work like plan9.
    Sample code:
    % cat x.go
    
    package main
    import "fmt"
    func main(){
    	fmt.Printf("HI!\n")
    }
    
    to build it as an ELF for harvey, with 2M alignment:
    GOOS=plan9 ../bin/go build -ldflags "-H linux -R 0x200000"  x.go
    
    os.test almost passes.
    To build a test
    GOOS=plan9 ../bin/go test -ldflags "-H linux -R 0x200000" -c os
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/syscall/asm_plan9_amd64.s
+++ src/syscall/asm_plan9_amd64.s
@@ -6,24 +6,26 @@
 #include "funcdata.h"
 
 //
-// System call support for Plan 9
+// // System call support for HarveyPlan 9System calls for AMD64, Linux
 //
+
+// Trap # in AX, args in DI SI DX R10 R8 R9, return in AX DX
+// Note that this differs from "standard" ABI convention, which
+// would pass 4th arg in CX, not R10.
 
 //func Syscall(trap, a1, a2, a3 uintptr) (r1, r2 uintptr, err string)
 //func Syscall6(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2 uintptr, err string)
 //func RawSyscall(trap, a1, a2, a3 uintptr) (r1, r2, err uintptr)
 //func RawSyscall6(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2, err uintptr)
-
-TEXT	·Syscall(SB),NOSPLIT,$0-64
+TEXT	·Syscall(SB),NOSPLIT,$0-56
 	CALL	runtime·entersyscall(SB)
-	MOVQ	trap+0(FP), BP	// syscall entry
-	// slide args down on top of system call number
-	LEAQ	a1+8(FP), SI
-	LEAQ	trap+0(FP), DI
-	CLD
-	MOVSQ
-	MOVSQ
-	MOVSQ
+	MOVQ	a1+8(FP), DI
+	MOVQ	a2+16(FP), SI
+	MOVQ	a3+24(FP), DX
+	MOVQ	$0, R10
+	MOVQ	$0, R8
+	MOVQ	$0, R9
+	MOVQ	trap+0(FP), AX	// syscall entry
 	SYSCALL
 	MOVQ	AX, r1+32(FP)
 	MOVQ	$0, r2+40(FP)
@@ -49,19 +51,16 @@ copyresult3:
 	CALL	runtime·exitsyscall(SB)
 	RET
 
-TEXT	·Syscall6(SB),NOSPLIT,$0-88
+// func Syscall6(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2, err uintptr)
+TEXT ·Syscall6(SB),NOSPLIT,$0-80
 	CALL	runtime·entersyscall(SB)
-	MOVQ	trap+0(FP), BP	// syscall entry
-	// slide args down on top of system call number
-	LEAQ	a1+8(FP), SI
-	LEAQ	trap+0(FP), DI
-	CLD
-	MOVSQ
-	MOVSQ
-	MOVSQ
-	MOVSQ
-	MOVSQ
-	MOVSQ
+	MOVQ	a1+8(FP), DI
+	MOVQ	a2+16(FP), SI
+	MOVQ	a3+24(FP), DX
+	MOVQ	a4+32(FP), R10
+	MOVQ	a5+40(FP), R8
+	MOVQ	a6+48(FP), R9
+	MOVQ	trap+0(FP), AX	// syscall entry
 	SYSCALL
 	MOVQ	AX, r1+56(FP)
 	MOVQ	$0, r2+64(FP)
@@ -87,66 +86,51 @@ copyresult4:
 	CALL	runtime·exitsyscall(SB)
 	RET
 
+// func RawSyscall(trap, a1, a2, a3 uintptr) (r1, r2, err uintptr)
 TEXT ·RawSyscall(SB),NOSPLIT,$0-56
-	MOVQ	trap+0(FP), BP	// syscall entry
-	// slide args down on top of system call number
-	LEAQ	a1+8(FP), SI
-	LEAQ	trap+0(FP), DI
-	CLD
-	MOVSQ
-	MOVSQ
-	MOVSQ
+	MOVQ	a1+8(FP), DI
+	MOVQ	a2+16(FP), SI
+	MOVQ	a3+24(FP), DX
+	MOVQ	$0, R10
+	MOVQ	$0, R8
+	MOVQ	$0, R9
+	MOVQ	trap+0(FP), AX	// syscall entry
 	SYSCALL
-	MOVQ	AX, r1+32(FP)
-	MOVQ	AX, r2+40(FP)
-	MOVQ	AX, err+48(FP)
-	RET
+	MOVQ	AX, r1+40(SP)
+	MOVQ	AX, r2+48(SP)
+	MOVQ	AX, err+56(SP)
+ 	RET
 
-TEXT	·RawSyscall6(SB),NOSPLIT,$0-80
-	MOVQ	trap+0(FP), BP	// syscall entry
-	// slide args down on top of system call number
-	LEAQ	a1+8(FP), SI
-	LEAQ	trap+0(FP), DI
-	CLD
-	MOVSQ
-	MOVSQ
-	MOVSQ
-	MOVSQ
-	MOVSQ
-	MOVSQ
+// func RawSyscall6(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2, err uintptr)
+TEXT ·RawSyscall6(SB),NOSPLIT,$0-80
+	MOVQ	a1+8(FP), DI
+	MOVQ	a2+16(FP), SI
+	MOVQ	a3+24(FP), DX
+	MOVQ	a4+32(FP), R10
+	MOVQ	a5+40(FP), R8
+	MOVQ	a6+48(FP), R9
+	MOVQ	trap+0(FP), AX	// syscall entry
 	SYSCALL
 	MOVQ	AX, r1+56(FP)
-	MOVQ	AX, r2+64(FP)
-	MOVQ	AX, err+72(FP)
+	MOVQ	DX, r2+64(FP)
+	MOVQ	$0, err+72(FP)
 	RET
 
-#define SYS_SEEK 39	/* from zsysnum_plan9_amd64.go */
+// func gettimeofday(tv *Timeval) (err uintptr)
+TEXT ·gettimeofday(SB),NOSPLIT,$0-16
+	MOVQ	tv+0(FP), DI
+	MOVQ	$0, SI
+	MOVQ	runtime·__vdso_gettimeofday_sym(SB), AX
+	CALL	AX
+
+	CMPQ	AX, $0xfffffffffffff001
+	JLS	ok7
+	NEGQ	AX
+	MOVQ	AX, err+8(FP)
+	RET
+ok7:
+	MOVQ	$0, err+8(FP)
+	RET
 
 //func seek(placeholder uintptr, fd int, offset int64, whence int) (newoffset int64, err string)
-TEXT ·seek(SB),NOSPLIT,$0-56
-	LEAQ	newoffset+32(FP), AX
-	MOVQ	AX, placeholder+0(FP)
-	
-	MOVQ	$SYS_SEEK, BP	// syscall entry
-	SYSCALL
-	
-	CMPL	AX, $-1
-	JNE	ok6
-	MOVQ	$-1, newoffset+32(FP)
-	
-	SUBQ	$16, SP
-	CALL	syscall·errstr(SB)
-	MOVQ	SP, SI
-	ADDQ	$16, SP	
-	JMP	copyresult6
-	
-ok6:
-	LEAQ	·emptystring(SB), SI
-	
-copyresult6:
-	LEAQ	err+40(FP), DI
-
-	CLD
-	MOVSQ
-	MOVSQ
-	RET
+//TEXT ·seek(SB),NOSPLIT,$0-56
