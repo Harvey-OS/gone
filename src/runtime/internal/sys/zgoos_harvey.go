bacc8aa6be5702f8e7ef41b744f920f5db448d6d
Sevki <s@sevki.org>
2017-06-18 23:22:05 +0200

    go/build: add harvey to syslist
    Signed-off-by: Sevki <s@sevki.org>

--- src/runtime/internal/sys/zgoos_harvey.go
+++ src/runtime/internal/sys/zgoos_harvey.go
@@ -2,15 +2,15 @@
 
 package sys
 
-const GOOS = `nacl`
+const GOOS = `harvey`
 
 const GoosAndroid = 0
 const GoosDarwin = 0
 const GoosDragonfly = 0
 const GoosFreebsd = 0
-const GoosHarvey = 0
+const GoosHarvey = 1
 const GoosLinux = 0
-const GoosNacl = 1
+const GoosNacl = 0
 const GoosNetbsd = 0
 const GoosOpenbsd = 0
 const GoosPlan9 = 0
